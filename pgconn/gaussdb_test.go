7444efb3e2f014214ca9086b45d7e56c36d2fee3
iseki <admin@iseki.space>
2024-08-08 20:26:02 +0800

    Fix test case

--- pgconn/gaussdb_test.go
+++ pgconn/gaussdb_test.go
@@ -13,7 +13,7 @@ func TestGaussGenerateKFromPBKDF2(t *testing.T) {
 	copy(b64[:], "0000000000000000000000000000000000000000000000000000000000000000")
 	copy(b8[:], "00000000")
 
-	r, e := gaussGenerateKFromPBKDF2("123456", b64, b8, b64, 2048)
+	r, e := gaussGenerateKFromPBKDF2("123456", b64, b8, b64, false, 2048)
 	assert.NoError(t, e)
 	fmt.Println(hex.EncodeToString(r))
 }
